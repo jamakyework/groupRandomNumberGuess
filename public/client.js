3cbe0aa9831ac9a6f5720828a84106b30d5a8a1f
John Amakye <john.amakye@minneapolismn.gov>
2016-12-09 08:41:21 -0600

    player guess display working

--- public/client.js
+++ public/client.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
   console.log("jquery is here");
-var postData = function () {
+//start postData
+var postData = function (){
+//start player object
 var playerObject = {
   player1:$("#player1").val(),
   player2:$("#player2").val(),
   player3:$("#player3").val(),
   player4:$("#player4").val()
 };//end player object
-
+//start ajax call
 $.ajax({
   type: 'POST',
   url: '/sendGuess',
@@ -18,38 +20,83 @@ $.ajax({
   error: function (){
     console.log('error with ajax call...');
   }
-});//end ajax call
+});
+//end ajax call
 };// end post data function
-
 var getData = function(){
   console.log("in get");
   $.ajax({
     type: "GET",
     url: "/testGet",
+    // data:?,
     success: function(response){
       console.log('back from post call:', response);
-      console.log('from response lenght:', response.length);
+      console.log('from response length:', response.length);
     $("#outputDiv").html("Total guesses:" + response.length);
-    for (var i = 0; i < response.length; i++) {
-      $("#outputDiv").html("Player Guesses:" + response[i])};
+
+function lastGuess(){
+var maximumNumber = 100;
+for (var i = 0; i < response.length; i++){
+// if else begin
+ if (response[i].player1 < maximumNumber) {
+$("#lastGuess1").html("Player 1 try again last guess was low");
+}
+else if (response[i].player1 > maximumNumber) {
+$("#lastGuess1").html("Player 1 try again last guess was high");
+}
+  else if (response[i].player1 === maximumNumber){
+  alert("Perfect guess, you win !!!");
+  ("#lastGuess1").html("perfect guess, you win !!!");
+}
+if (response[i].player2 < maximumNumber) {
+$("#lastGuess2").html("Player 2 try again last guess was low");
+}
+else if (response[i].player2 > maximumNumber) {
+$("#lastGuess2").html("Player 2 try again last guess was high");
+}
+if (response[i].player3 < maximumNumber) {
+$("#lastGuess3").html("Player 3 try again last guess was low");
+}
+else if (response[i].player3 > maximumNumber) {
+$("#lastGuess3").html("Player 3 try again last guess was high");
+}
+if (response[i].player4 < maximumNumber) {
+$("#lastGuess4").html("Player 4 try again last guess was low");
+}
+else if (response[i].player4 > maximumNumber) {
+$("#lastGuess4").html("Plaer 4 try again last guess was high");
+}
+  $("#player1").val("");
+  $("#player2").val("");
+  $("#player3").val("");
+  $("#player4").val("");
+//if else end
+}//end for
+}//end guess
+lastGuess();
   },
     error: function (){
       console.log('error with ajax call...');
-    }
-  });//end ajax call
-};// end get data function
-
+  }
+});//end ajax call
+};//end get data function
+//start submit data
 $('#submitData').on('click', function (){
 console.log("in submit data click");
 postData();
 getData();
-
-$("#player1").val("");
-$("#player2").val("");
-$("#player3").val("");
-$("#player4").val("");
-
 });//end submit data
-
-
 });//end doc ready
+
+
+//   //start lastGuess for loop
+//   function lastGuess(){for (var i = 0; i < response.length; i++) {
+//   $("#lastGuess1").html("Player 1 last guess:" + response[i].player1);
+//   $("#lastGuess2").html("Player 2 last guess:" + response[i].player2);
+//   $("#lastGuess3").html("Player 3 last guess:" + response[i].player3);
+//   $("#lastGuess4").html("Player 4 last guess:" + response[i].player4);
+//   $("#player1").val("");
+//   $("#player2").val("");
+//   $("#player3").val("");
+//   $("#player4").val("");
+//    }//end lastGuess for loop
--- server/app.js
+++ server/app.js
@@ -4,19 +4,14 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var urlEncodedParser = bodyParser.urlencoded ({extended: false});
 var port = process.env.PORT || 8080;
-
 var playerGuessArray = [];
-
 
 app.listen (port, function (req,res){
   console.log( 'server is listening on', port);
 });//end spin up server
 
-//base url
-app.get('/', function(req, res){
-  console.log('base url hit');
-  res.sendFile(path.resolve('public/index.html'));
-});//end base url
+//static folder
+app.use( express.static('public'));
 
 //base url
 app.get('/testGet', function(req, res){
@@ -24,15 +19,17 @@ app.get('/testGet', function(req, res){
   res.send(playerGuessArray);
 });//end base url
 
+//start post
 app.post('/sendGuess', urlEncodedParser, function ( req, res){
   console.log('send guess url hit', req.body);
   var playerGuess = req.body;
   playerGuessArray.push(playerGuess);
   console.log(playerGuessArray);
   console.log("This is the number of guesses?:",playerGuessArray.length);
-
 });//end post
 
-
-//static folder
-app.use( express.static('public'));
+//start get
+app.get('/', function(req, res){
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/index.html'));
+});//end get
