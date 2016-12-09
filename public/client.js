0bcd0518f20dca0e54ae3d6dbbdbc93b5980af56
Elliot Thomas <elliotnthomas@gmail.com>
2016-12-09 10:29:08 -0600

    game win alert and abandon button function

--- public/client.js
+++ public/client.js
@@ -1,6 +1,29 @@
+var maxNumber = $('#selectMaxNumber').val();
+
 $(document).ready(function() {
   console.log("jquery is here");
+
+var postMaxNumber = function () {
+
+  var maxNumber = {
+    maxNumber: $('#selectMaxNumber').val()
+  };
+
+$.ajax({
+    type: 'POST',
+    url: '/sendMax',
+    data: maxNumber,
+    success: function(response){
+      console.log('back from post call:', response);
+    },
+    error: function (){
+      console.log('error with ajax call...');
+    }
+  });//end ajax call
+  };// end post data function
+
 var postData = function () {
+
 var playerObject = {
   player1:$("#player1").val(),
   player2:$("#player2").val(),
@@ -28,8 +51,34 @@ var getData = function(){
     url: "/testGet",
     success: function(response){
       console.log('back from post call:', response);
-      console.log('from response lenght:', response.length);
+      // console.log('from response lenght:', response.length);
     $("#outputDiv").html("Total guesses:" + response.length);
+    var player1 = $("#lastGuess1");
+    var player2 = $("#lastGuess2");
+    var player3 = $("#lastGuess3");
+    var player4 = $("#lastGuess4");
+
+    var player1Response = response[response.length - 1].player1
+    var player2Response = response[response.length - 1].player2
+    var player3Response = response[response.length - 1].player3
+    var player4Response = response[response.length - 1].player4
+    console.log(player1.response);
+    if (player1Response === "Player 1 is correct!"){
+      alert("Congrats! You win, Player 1!")
+    } else if (player2Response === "Player 2 is correct!") {
+      alert("Congrats! You win, Player 2!")
+    } else if (player3Response === "Player 3 is correct!") {
+      alert("Congrats! You win, Player 3!")
+    } else if (player4Response === "Player 4 is correct!") {
+      alert("Congrats! You win, Player 4!")
+    } else {
+      player1.html("<p>" + player1Response + "</p>" );
+      player2.html("<p>" + player2Response + "</p>" );
+      player3.html("<p>" + player3Response + "</p>" );
+      player4.html("<p>" + player4Response + "</p>" );
+    };
+
+    console.log(player1Response);
   },
     error: function (){
       console.log('error with ajax call...');
@@ -38,16 +87,57 @@ var getData = function(){
 };// end get data function
 
 $('#submitData').on('click', function (){
-console.log("in submit data click");
-postData();
-getData();
+  console.log("in submit data click");
+  postData();
+  getData();
 
-$("#player1").val("");
-$("#player2").val("");
-$("#player3").val("");
-$("#player4").val("");
+  $("#player1").val("");
+  $("#player2").val("");
+  $("#player3").val("");
+  $("#player4").val("");
 
 });//end submit data
 
+$('#submitMaxNumber').on('click', function (){
+  postMaxNumber();
+
+});//end submit max number function
+
+$("#startGame").on('click', function (){
+  $("#inputFields").show();
+  $("#submitData").show();
+  $("#secondHeading").show();
+  $("#selectMaxNumber").hide();
+  $("#abandonGameButton").show();
+  $("#startGame").hide();
+  $("#firstHeading").hide();
+  $("#thirdHeading").hide();
+  $("#submitMaxNumber").hide();
+  showMaxNumber();
+});//end start game on click
+
+$("#inputFields").hide();
+$("#submitData").hide();
+$("#secondHeading").hide();
+$("#abandonGameButton").hide();
+
+$("#abandonGameButton").on('click', function (){
+  $("#inputFields").hide();
+  $("#submitData").hide();
+  $("#secondHeading").hide();
+  $("#selectMaxNumber").show();
+  $("#abandonGameButton").hide();
+  $("#startGame").show();
+  $("#firstHeading").show();
+  $("#thirdHeading").show();
+  $("#submitMaxNumber").show();
+  //showMaxNumber();
+});//end abandonGameButton
+
+var showMaxNumber = function (){
+  var maxNumber = $('#selectMaxNumber').val();
+  var maxNumberPlace = $('#secondHeading')
+  maxNumberPlace.html("EACH PLAYER GUESS A NUMBER BUT NOT OVER THE MAXIMUM NUMBER..." + maxNumber);
+};
 
 });//end doc ready
--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ var urlEncodedParser = bodyParser.urlencoded ({extended: false});
 var port = process.env.PORT || 8080;
 
 var playerGuessArray = [];
+var randomNumbersArray = [];
+var answerArray = [];
 
 
 app.listen (port, function (req,res){
@@ -18,21 +20,93 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve('public/index.html'));
 });//end base url
 
-//base url
+//send info back
 app.get('/testGet', function(req, res){
   console.log('base url hit');
-  res.send(playerGuessArray);
-});//end base url
+  res.send(answerArray);
+});//send info back
+
+app.post('/sendMax', urlEncodedParser, function ( req, res){
+  console.log('send guess url hit', req.body);
+  var num = req.body;
+  var maxNumValue = num.maxNumber;
+  var randomNumber = Math.floor(Math.random() * maxNumValue) + 1
+  console.log("random number is", randomNumber);
+  randomNumbersArray.push(randomNumber);
+
+  // maxNumberArray.push(maxNumber);
+  // console.log("max number array value is", maxNumberArray[maxNumberArray.length - 1].maxNumber);
+
+
+});//end post
 
 app.post('/sendGuess', urlEncodedParser, function ( req, res){
   console.log('send guess url hit', req.body);
   var playerGuess = req.body;
   playerGuessArray.push(playerGuess);
-  console.log(playerGuessArray);
+  console.log("player 1 value!!!!", playerGuessArray[playerGuessArray.length - 1].player1);
   console.log("This is the number of guesses?:",playerGuessArray.length);
+  isNumCorrect(playerGuessArray);
 
 });//end post
 
 
 //static folder
 app.use( express.static('public'));
+
+function isNumCorrect (array){
+  player1 = array[array.length - 1].player1;
+  player2 = array[array.length - 1].player2;
+  player3 = array[array.length - 1].player3;
+  player4 = array[array.length - 1].player4;
+
+  var randomNum = randomNumbersArray[randomNumbersArray.length - 1]
+  var player1answer = '';
+  var player2answer = '';
+  var player3answer = '';
+  var player4answer = '';
+
+  if (player1 > randomNum){
+    player1answer = "High";
+  } else if (player1 < randomNum){
+    player1answer = "Low";
+  } else {
+    player1answer = "Player 1 is correct!";
+  }
+
+  if (player2 > randomNum){
+    player2answer = "High";
+  } else if (player2 < randomNum){
+    player2answer = "Low";
+  } else {
+    player2answer = "Player 2 is correct!";
+  }
+
+  if (player3 > randomNum){
+    player3answer = "High";
+  } else if (player3 < randomNum){
+    player3answer = "Low";
+  } else {
+    player3answer = "Player 3 is correct!";
+  }
+
+  if (player4 > randomNum){
+    player4answer = "High";
+  } else if (player4 < randomNum){
+    player4answer = "Low";
+  } else {
+    player4answer = "Player 4 is correct!";
+  }
+
+  var objectToReturn = {
+    player1: player1answer,
+    player2: player2answer,
+    player3: player3answer,
+    player4: player4answer
+  };
+
+  answerArray.push(objectToReturn);
+
+  console.log("you are looking at the answer array:", answerArray);
+
+};//end is num correct function
